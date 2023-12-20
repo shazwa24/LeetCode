3eb95003d3330b685299b55dd0f4b8c8793a7b90
Shazwa tun Naeem <108590521+shazwa24@users.noreply.github.com>
2023-12-20 15:41:58 +0500

    Time: 53 ms (50.48%), Space: 42.6 MB (6.58%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+        l=0
+    for (let i = 0; i < nums.length; i++)
+    {
+        if(nums[i] != val)
+        {
+            nums[l] = nums[i]
+            l++;
+        }
+    }
+    return l
+};
